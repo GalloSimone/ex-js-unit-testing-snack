4da4ee84d434b1458d1f8109f0d29ec1c5c86a1d
GalloSimone <gallosimonecz@gmail.com>
2025-04-04 15:32:48 +0200

    snack 2 3 4 completati

--- snack.js
+++ snack.js
@@ -4,4 +4,27 @@ function getInitials(nomeCompleto){
     const [nome, cognome]= nomeCompleto.split(" ");
     return `${nome.charAt(0)}.${cognome.charAt(0)}. ` ;
 }
-module.exports = {getInitials,}
+// 🏆 Snack 2
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+function createSlug(string){
+     return string.toLowerCase().replaceAll(" ","-")
+     
+}
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+function average(numeri){
+    let somma= 0;
+    numeri.forEach(numero => {
+    somma += numero;    
+    });
+ return somma / numeri.length;
+
+}
+// 🏆 Snack 4
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug sostituisce gli spazi con -."
+
+
+module.exports = {getInitials,createSlug,average}
--- snack.test.js
+++ snack.test.js
@@ -1,4 +1,4 @@
-const {getInitials,}=require("./snack.js")
+const {getInitials,createSlug,average}=require("./snack.js")
 
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
@@ -9,3 +9,25 @@ test('La funzione getInitials restituisce le iniziali di un nome completo ', () 
     expect(getInitials("Mario Rossi")).toBe("M.R. ");
  })
 
+//  🏆 Snack 2
+//  Creare un test che verifichi la seguente descrizione:
+//  👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+
+test ("La funzione createSlug restituisce una stringa in lowercase",()=>{
+    expect(createSlug("BOOLEAN")).toBe("boolean")
+})
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+
+test("La funzione average calcola la media aritmetica di un array di numeri",()=>{
+    expect(average([10,20])).toBe(15)
+})
+// 🏆 Snack 4
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug sostituisce gli spazi con -."
+test("La funzione createSlug sostituisce gli spazi con -.",()=>{
+    expect(createSlug('questo è un test')).toBe("questo-è-un-test")
+})
